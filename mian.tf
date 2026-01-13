dbd0fb341fd7df088f767a69434ee8a257f6f456
Delightsome Asolo <delightsomeasolo@gmail.com>
2026-01-13 23:37:01 +0100

    security group

--- mian.tf
+++ mian.tf
@@ -3,4 +3,13 @@ module "vpc" {
   version = "6.6.0"
 
   name = var.vpc_name
+  cidr = var.cidr_block
+}
+
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+
+  vpc_id = module.vpc.vpc_id
+  name   = "my cool security group"
 }
